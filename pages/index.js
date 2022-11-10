d4e8b6b813fcc8e83e15f0cdbea072d1d1f62a64
lsmb <lsmbc@pm.me>
2022-11-10 22:25:31 +0200

    Initial

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import Date from '../components/date'
+
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  console.log(allPostsData)
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p></p>
+        <p>
+          A simple portfolio page
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <div className={utilStyles.wrapper}>
+          {allPostsData.map(({ id, date, title, github }) => (
+            <div className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} /> {}
+              </small>
+              <a href={github} target="_blank"><svg xmlns={github} width="23" height="24" viewBox="0 0 24 24"><path fill="currentcolor" color="#346c7c" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg></a>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  )
+}
